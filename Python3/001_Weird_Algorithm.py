ec8137496f9e682527d790ac61877fb5af332475
abavoil <abavoil@ASUS-GL502VM.localdomain>
2020-08-09 18:39:31 +0200

    weird algorithm python submission

---
+++ Python3/001_Weird_Algorithm.py
@@ -0,0 +1,10 @@
+def main():
+    n = int(input())
+    while n != 1:
+        print(n, end=" ")
+        n = n // 2 if n % 2 == 0 else 3 * n + 1
+    print(1)
+
+
+if __name__ == '__main__':
+    main()
